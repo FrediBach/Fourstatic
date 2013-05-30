b6457a3f0d8fd2bea9a0427afd0d13cfee828df7
Fredi Bach <osxcode@gmail.com>
2013-05-30 11:46:17 +0200

    v0.1.4
    Made autolinked links open in new tab.

--- fourstatic/fourstatic.twig.php
+++ fourstatic/fourstatic.twig.php
@@ -42,8 +42,8 @@ $filter = new Twig_SimpleFilter('autolink', function ($string) {
 	
 	if (is_string($string)){
 		$ret = ' ' . $string;
-	    $ret = preg_replace("#(^|[\n ])([\w]+?://[\w]+[^ \"\n\r\t<]*)#ise", "'\\1<a href=\"\\2\" >\\2</a>'", $ret);
-	    $ret = preg_replace("#(^|[\n ])((www|ftp)\.[^ \"\t\n\r<]*)#ise", "'\\1<a href=\"http://\\2\" >\\2</a>'", $ret);
+	    $ret = preg_replace("#(^|[\n ])([\w]+?://[\w]+[^ \"\n\r\t<]*)#ise", "'\\1<a href=\"\\2\" target=\"_blank\">\\2</a>'", $ret);
+	    $ret = preg_replace("#(^|[\n ])((www|ftp)\.[^ \"\t\n\r<]*)#ise", "'\\1<a href=\"http://\\2\" target=\"_blank\">\\2</a>'", $ret);
 	    $ret = preg_replace("#(^|[\n ])([a-z0-9&\-_\.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#i", "\\1<a href=\"mailto:\\2@\\3\">\\2@\\3</a>", $ret);
 	    $ret = substr($ret, 1);
 		$string = $ret;
